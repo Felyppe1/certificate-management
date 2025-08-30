f2a457e07c5d484a1e589af705aea85fcc01b38d
Felyppe1 <felyppe.nunes1@gmail.com>
2025-08-30 10:42:15 -0300

    refactor: login with google

--- src/app/api/auth/google/callback/route.ts
+++ src/app/api/auth/google/callback/route.ts
@@ -1,99 +1,34 @@
-import { ExternalUserAccount } from '@/backend/application/interfaces/external-user-account'
-import { User } from '@/backend/application/interfaces/users-repository'
 import { PrismaExternalUserAccountsRepository } from '@/backend/infrastructure/repository/prisma/prisma-external-user-accounts-repository'
 import { PrismaUsersRepository } from '@/backend/infrastructure/repository/prisma/prisma-users-repository'
-import { OAuth2Client } from 'google-auth-library'
 import { NextResponse } from 'next/server'
-import crypto from 'crypto'
 import { PrismaSessionsRepository } from '@/backend/infrastructure/repository/prisma/prisma-sessions-repository'
 import { cookies } from 'next/headers'
-
-const oAuth2Client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.NEXT_PUBLIC_BASE_URL + '/api/auth/google/callback',
-)
+import { LoginGoogleUseCase } from '@/backend/application/login-google-use-case'
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
-    console.log(searchParams)
+
     const code = searchParams.get('code')
 
     if (!code) {
-        console.log('No code provided')
         return NextResponse.redirect(
             process.env.NEXT_PUBLIC_BASE_URL + '/entrar',
         )
     }
-
-    const { tokens } = await oAuth2Client.getToken(code)
-
-    console.log('Tokens received:', tokens)
-
-    const ticket = await oAuth2Client.verifyIdToken({
-        idToken: tokens.id_token!,
-        audience: process.env.GOOGLE_CLIENT_ID,
-    })
-
-    const payload = ticket.getPayload()
-
-    console.log('Payload:', payload)
 
     const usersRepository = new PrismaUsersRepository()
     const externalUserAccountsRepository =
         new PrismaExternalUserAccountsRepository()
     const sessionsRepository = new PrismaSessionsRepository()
 
-    if (payload?.email) {
-        const userExists = await usersRepository.getByEmail(payload.email)
+    const loginGoogleUseCase = new LoginGoogleUseCase(
+        usersRepository,
+        externalUserAccountsRepository,
+        sessionsRepository,
+    )
 
-        let newUser: User
-
-        if (!userExists) {
-            const newUser: User = {
-                id: crypto.randomUUID(),
-                email: payload.email,
-                name: payload.name!,
-                passwordHash: null,
-            }
-
-            await usersRepository.save(newUser)
-        }
-
-        const user = userExists ? userExists : newUser!
-
-        const externalAccount = await externalUserAccountsRepository.getById(
-            user.id,
-            'GOOGLE',
-        )
-
-        if (!externalAccount) {
-            const newExternalAccount: ExternalUserAccount = {
-                userId: user.id,
-                provider: 'GOOGLE',
-                providerUserId: payload.sub!,
-                accessToken: tokens.access_token!,
-                refreshToken: tokens.refresh_token ?? null,
-                accessTokenExpiryDateTime: null,
-                refreshTokenExpiryDateTime: null,
-            }
-
-            await externalUserAccountsRepository.save(newExternalAccount)
-        } else {
-            externalAccount.accessToken = tokens.access_token!
-            externalAccount.refreshToken = tokens.refresh_token ?? null
-            externalAccount.accessTokenExpiryDateTime = null
-            externalAccount.refreshTokenExpiryDateTime = null
-
-            await externalUserAccountsRepository.update(externalAccount)
-        }
-
-        const sessionToken = crypto.randomBytes(32).toString('hex')
-
-        await sessionsRepository.save({
-            userId: user.id,
-            token: sessionToken,
-        })
+    try {
+        const sessionToken = await loginGoogleUseCase.execute({ code })
 
         const cookie = await cookies()
 
@@ -103,8 +38,11 @@ export async function GET(request: Request) {
             // secure: true,
             // sameSite: "strict"
         })
-    }
 
-    // oAuth2Client.setCredentials(tokens);
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL + '/')
+        return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL + '/')
+    } catch (error) {
+        return NextResponse.redirect(
+            process.env.NEXT_PUBLIC_BASE_URL + '/entrar',
+        )
+    }
 }
---
+++ src/backend/application/login-google-use-case.ts
@@ -0,0 +1,106 @@
+import { OAuth2Client } from 'google-auth-library'
+import {
+    ExternalUserAccount,
+    ExternalUserAccountsRepository,
+} from './interfaces/external-user-account'
+import { SessionsRepository } from './interfaces/sessions-repository'
+import { User, UsersRepository } from './interfaces/users-repository'
+import crypto from 'crypto'
+
+interface LoginGoogleUseCaseInput {
+    code: string
+}
+
+export class LoginGoogleUseCase {
+    constructor(
+        private usersRepository: Pick<UsersRepository, 'save' | 'getByEmail'>,
+        private externalUserAccountsRepository: Pick<
+            ExternalUserAccountsRepository,
+            'save' | 'update' | 'getById'
+        >,
+        private sessionsRepository: Pick<SessionsRepository, 'save'>,
+    ) {}
+
+    async execute({ code }: LoginGoogleUseCaseInput) {
+        const oAuth2Client = new OAuth2Client({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            redirectUri:
+                process.env.NEXT_PUBLIC_BASE_URL + '/api/auth/google/callback',
+        })
+
+        const { tokens } = await oAuth2Client.getToken(code)
+
+        console.log('Tokens received:', tokens)
+
+        if (
+            !tokens.scope?.includes(
+                'https://www.googleapis.com/auth/drive.file',
+            )
+        ) {
+            throw new Error('Unauthorized')
+        }
+
+        const ticket = await oAuth2Client.verifyIdToken({
+            idToken: tokens.id_token!,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        })
+
+        const payload = ticket.getPayload()
+
+        console.log('Payload:', payload)
+
+        const email = payload!.email!
+
+        const userExists = await this.usersRepository.getByEmail(email)
+
+        let newUser: User
+
+        if (!userExists) {
+            newUser = {
+                id: crypto.randomUUID(),
+                email: email,
+                name: payload!.name!,
+                passwordHash: null,
+            }
+
+            await this.usersRepository.save(newUser)
+        }
+
+        const user = userExists ?? newUser!
+
+        const externalAccount =
+            await this.externalUserAccountsRepository.getById(user.id, 'GOOGLE')
+
+        if (!externalAccount) {
+            const newExternalAccount: ExternalUserAccount = {
+                userId: user.id,
+                provider: 'GOOGLE',
+                providerUserId: payload!.sub!,
+                accessToken: tokens.access_token!,
+                refreshToken: tokens.refresh_token ?? null,
+                // TODO: calculate expiry date times
+                accessTokenExpiryDateTime: null,
+                refreshTokenExpiryDateTime: null,
+            }
+
+            await this.externalUserAccountsRepository.save(newExternalAccount)
+        } else {
+            externalAccount.accessToken = tokens.access_token!
+            externalAccount.refreshToken = tokens.refresh_token ?? null
+            externalAccount.accessTokenExpiryDateTime = null
+            externalAccount.refreshTokenExpiryDateTime = null
+
+            await this.externalUserAccountsRepository.update(externalAccount)
+        }
+
+        const sessionToken = crypto.randomBytes(32).toString('hex')
+
+        await this.sessionsRepository.save({
+            userId: user.id,
+            token: sessionToken,
+        })
+
+        return sessionToken
+    }
+}
