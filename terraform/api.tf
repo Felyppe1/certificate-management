f39bf228032f368904dde3347cc9e8d73be3dc36
Felyppe1 <felyppe.nunes1@gmail.com>
2025-10-11 21:13:40 -0300

    fix: adding new apis to activate

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,13 +1,20 @@
 resource "google_project_service" "gcp_services" {
   for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
     "drive.googleapis.com",
     "slides.googleapis.com",
     "picker.googleapis.com",
     "run.googleapis.com",
     "cloudbuild.googleapis.com",
     "artifactregistry.googleapis.com",
-    "iam.googleapis.com",
-    "cloudresourcemanager.googleapis.com"
+    "secretmanager.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "monitoring.googleapis.com",
+    "logging.googleapis.com",
+    "storage.googleapis.com",
+    "pubsub.googleapis.com",
   ])
 
   project  = var.project_id
