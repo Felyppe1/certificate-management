c23fca9687907734b3e484c85733e79ddc529eaf
Felyppe1 <felyppe.nunes1@gmail.com>
2025-11-06 17:44:50 -0300

    refactor: from cloud function to cloud run

--- terraform/cloud-function.tf
+++ terraform/cloud-function.tf
@@ -1,87 +1,87 @@
-data "archive_file" "generate_pdfs_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../cloud-functions/generate-pdfs"
-  output_path = "${path.module}/../cloud-functions/builds/generate-pdfs.zip"
+# data "archive_file" "generate_pdfs_zip" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/../cloud-functions/generate-pdfs"
+#   output_path = "${path.module}/../cloud-functions/builds/generate-pdfs.zip"
 
-  excludes = [
-    ".git",
-    ".gitignore",
-    "__pycache__",
-    "*.pyc",
-    ".pytest_cache",
-    "venv",
-    ".venv",
-  ]
-}
-
-resource "google_storage_bucket_object" "generate_pdfs_object" {
-  name   = "generate-pdfs${local.suffix}-${data.archive_file.generate_pdfs_zip.output_md5}.zip"
-  bucket = google_storage_bucket.cloud_functions.name
-  content_type = "application/zip"
-  source = data.archive_file.generate_pdfs_zip.output_path
-
-  depends_on = [ data.archive_file.generate_pdfs_zip ]
-}
-
-resource "google_cloudfunctions2_function" "generate_pdfs_function" {
-  name     = "generate-pdfs${local.suffix}"
-  location = var.region
-  
-  build_config {
-    # runtime     = "python311"
-    entry_point = "main"
-    runtime = "custom"
-    
-    # O Terraform vai usar Cloud Build automaticamente para buildar o Docker
-    source {
-      storage_source {
-        bucket = google_storage_bucket.cloud_functions.name
-        object = google_storage_bucket_object.generate_pdfs_object.name
-      }
-    }
-    
-    docker_repository = google_artifact_registry_repository.cloud_functions_repository.id
-    
-    # Variáveis de ambiente durante o build (se necessário)
-    # environment_variables = {
-    #   BUILD_VAR = "value"
-    # }
-  }
-  
-  service_config {
-    max_instance_count = 10
-    min_instance_count = 0
-    available_memory   = "256M"
-    timeout_seconds    = 240
-    
-    environment_variables = {
-      APP_BASE_URL        = "https://${google_cloud_run_v2_service.app.name}-${data.google_project.project.number}.${google_cloud_run_v2_service.app.location}.run.app"
-      SOFFICE_PATH        = "/usr/bin/soffice"
-      CERTIFICATES_BUCKET = google_storage_bucket.certificates.name
-    }
-    
-    service_account_email = google_service_account.app_service_account.email
-  }
-
-  depends_on = [ google_storage_bucket_object.generate_pdfs_object ]
-}
-
-resource "google_artifact_registry_repository" "cloud_functions_repository" {
-  location      = var.region
-  repository_id = "cloud-functions${local.suffix}"
-  format        = "DOCKER"
-  
-  description = "Repository for Cloud Functions container images"
-}
-
-# They are not public by default, so we need to add this IAM member
-resource "google_cloud_run_service_iam_member" "public_invoker" {
-  location = google_cloudfunctions2_function.generate_pdfs_function.location
-  service  = google_cloudfunctions2_function.generate_pdfs_function.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
-# output "function_uri" {
-#   value = google_cloudfunctions2_function.generate_pdfs_function.service_config[0].uri
+#   excludes = [
+#     ".git",
+#     ".gitignore",
+#     "__pycache__",
+#     "*.pyc",
+#     ".pytest_cache",
+#     "venv",
+#     ".venv",
+#   ]
 # }
+
+# resource "google_storage_bucket_object" "generate_pdfs_object" {
+#   name   = "generate-pdfs${local.suffix}-${data.archive_file.generate_pdfs_zip.output_md5}.zip"
+#   bucket = google_storage_bucket.cloud_functions.name
+#   content_type = "application/zip"
+#   source = data.archive_file.generate_pdfs_zip.output_path
+
+#   depends_on = [ data.archive_file.generate_pdfs_zip ]
+# }
+
+# resource "google_cloudfunctions2_function" "generate_pdfs_function" {
+#   name     = "generate-pdfs${local.suffix}"
+#   location = var.region
+  
+#   build_config {
+#     # runtime     = "python311"
+#     entry_point = "main"
+#     runtime = "custom"
+
+#     # O Terraform vai usar Cloud Build automaticamente para buildar o Docker
+#     source {
+#       storage_source {
+#         bucket = google_storage_bucket.cloud_functions.name
+#         object = google_storage_bucket_object.generate_pdfs_object.name
+#       }
+#     }
+
+#     docker_repository = google_artifact_registry_repository.cloud_functions_repository.id
+    
+#     # Variáveis de ambiente durante o build (se necessário)
+#     # environment_variables = {
+#     #   BUILD_VAR = "value"
+#     # }
+#   }
+  
+#   service_config {
+#     max_instance_count = 10
+#     min_instance_count = 0
+#     available_memory   = "256M"
+#     timeout_seconds    = 240
+    
+#     environment_variables = {
+#       APP_BASE_URL        = "https://${google_cloud_run_v2_service.app.name}-${data.google_project.project.number}.${google_cloud_run_v2_service.app.location}.run.app"
+#       SOFFICE_PATH        = "/usr/bin/soffice"
+#       CERTIFICATES_BUCKET = google_storage_bucket.certificates.name
+#     }
+    
+#     service_account_email = google_service_account.app_service_account.email
+#   }
+
+#   depends_on = [ google_storage_bucket_object.generate_pdfs_object ]
+# }
+
+# resource "google_artifact_registry_repository" "cloud_functions_repository" {
+#   location      = var.region
+#   repository_id = "cloud-functions${local.suffix}"
+#   format        = "DOCKER"
+  
+#   description = "Repository for Cloud Functions container images"
+# }
+
+# # They are not public by default, so we need to add this IAM member
+# resource "google_cloud_run_service_iam_member" "public_invoker" {
+#   location = google_cloudfunctions2_function.generate_pdfs_function.location
+#   service  = google_cloudfunctions2_function.generate_pdfs_function.name
+#   role     = "roles/run.invoker"
+#   member   = "allUsers"
+# }
+
+# # output "function_uri" {
+# #   value = google_cloudfunctions2_function.generate_pdfs_function.service_config[0].uri
+# # }
--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -55,8 +55,8 @@ resource "google_cloud_run_v2_service" "app" {
       }
 
       env {
-        name = "CLOUD_FUNCTION_BASE_URL"
-        value = "https://${var.region}-${data.google_project.project.project_id}.cloudfunctions.net"
+        name = "GENERATE_PDFS_URL"
+        value = google_cloud_run_v2_service.generate_pdfs.uri
       }
 
       env {
@@ -80,6 +80,7 @@ resource "google_cloud_run_v2_service" "app" {
   }
 }
 
+# They are not public by default, so we need to add this IAM member
 resource "google_cloud_run_v2_service_iam_member" "public_access" {
   project  = var.project_id
   location = google_cloud_run_v2_service.app.location
@@ -97,3 +98,101 @@ output "cloud_run_name" {
   value       = google_cloud_run_v2_service.app.name
   description = "Cloud Run service name"
 }
+
+
+
+
+
+
+
+
+
+resource "google_cloud_run_v2_service" "generate_pdfs" {
+  name     = "generate-pdfs${local.suffix}"
+  location = var.region
+  project  = var.project_id
+
+  deletion_protection = false
+
+  depends_on = [
+    google_project_service.gcp_services
+  ]
+
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+    service_account = google_service_account.app_service_account.email
+
+    # scaling {
+    #   min_instance_count = 0
+    #   max_instance_count = 1
+    # }
+
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+      ports {
+        container_port = 8080
+      }
+
+      # resources {
+      #   limits = {
+      #     cpu    = "2"
+      #     memory = "1Gi"
+      #   }
+      # }
+
+      dynamic "env" {
+        for_each = var.environment_variables
+        content {
+          name  = env.key
+          value = env.value
+        }
+      }
+
+      env {
+        name = "APP_BASE_URL"
+        value = "https://${google_cloud_run_v2_service.app.name}-${data.google_project.project.number}.${google_cloud_run_v2_service.app.location}.run.app"
+      }
+
+      env {
+        name  = "CERTIFICATES_BUCKET"
+        value = google_storage_bucket.certificates.name
+      }
+
+      env {
+        name = "SOFFICE_PATH"
+        value = "/usr/bin/soffice"
+      }
+    }
+
+    timeout = "600s"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image
+    ]
+  }
+}
+
+# This is not necessary because the SA already has the permission in the project scope.
+# However, for clarity, we add it here to show that the app service can invoke the generate_pdfs service.
+# This permission is in the service scope.
+resource "google_cloud_run_v2_service_iam_member" "allow_app_to_invoke_generate_pdfs" {
+  project  = var.project_id
+  location = google_cloud_run_v2_service.generate_pdfs.location
+  name     = google_cloud_run_v2_service.generate_pdfs.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.app_service_account.email}"
+}
+
+output "generate_pdfs_cloud_run_name" {
+  value       = google_cloud_run_v2_service.generate_pdfs.name
+  description = "Cloud Run service name for generating PDFs of certificates"
+}
+
+# output "generate_pdfs_cloud_run_url" {
+#   value = "https://${google_cloud_run_v2_service.generate_pdfs.name}-${data.google_project.project.number}.${google_cloud_run_v2_service.generate_pdfs.location}.run.app"
+#   description = "Cloud Run URL for generating PDFs of certificates"
+# }
