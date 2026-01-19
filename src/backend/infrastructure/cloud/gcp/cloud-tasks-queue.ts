aa6ec9a7c073901c92060c80c2edcb32b2c53eb8
Felyppe1 <felyppe.nunes1@gmail.com>
2026-01-19 07:49:32 -0300

    fix: sending to queue

--- src/backend/infrastructure/cloud/gcp/cloud-tasks-queue.ts
+++ src/backend/infrastructure/cloud/gcp/cloud-tasks-queue.ts
@@ -17,12 +17,13 @@ export class CloudTasksQueue implements IQueue {
     ): Promise<void> {
         const project = process.env.GCP_PROJECT_ID!
         const location = process.env.GCP_REGION!
-        const queue = process.env.CERTIFICATE_GENERATIONS_QUEUE_NAME!
+        const queue = `certificate-generations${process.env.SUFFIX || ''}`
+        const url = this.getCloudFunctionUrl('generate-pdfs')
 
         const parent = this.queue.queuePath(project, location, queue)
         const task = {
             httpRequest: {
-                relativeUri: '/',
+                url,
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -38,5 +39,21 @@ export class CloudTasksQueue implements IQueue {
         data: EnqueueSendCertificateEmailsInput,
     ): Promise<void> {
         throw new Error('Method not implemented.')
+    }
+
+    private getCloudFunctionUrl(functionName: string): string {
+        const projectNumber = process.env.GCP_PROJECT_NUMBER
+        const region = process.env.GCP_REGION
+        const suffix = process.env.SUFFIX || ''
+
+        if (!projectNumber || !region) {
+            throw new Error('GCP_PROJECT_NUMBER or GCP_REGION not set')
+        }
+
+        const functionFullName = `${functionName}${suffix}`
+
+        const url = `https://${functionFullName}-${projectNumber}.${region}.run.app`
+
+        return url
     }
 }
