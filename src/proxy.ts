b2ebbf60b8f43d848560e6ba5b99ec78e41631b7
Felyppe1 <felyppe.nunes1@gmail.com>
2026-04-12 11:42:43 -0300

    refactor: middleware to proxy

---
+++ src/proxy.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const publicRoutes = ['/entrar', '/cadastrar-se']
+
+export function proxy(request: NextRequest) {
+    const { pathname } = request.nextUrl
+    console.log('middleware triggered by request to:', pathname)
+
+    const sessionToken = request.cookies.get('session_token')?.value
+    console.log('Session token from cookie:', sessionToken)
+
+    const isPublicRoute = publicRoutes.includes(pathname)
+
+    if (sessionToken) {
+        if (isPublicRoute) {
+            return NextResponse.redirect(new URL('/', request.url))
+        }
+
+        return NextResponse.next()
+    }
+
+    const isServerAction = request.headers.has('next-action')
+    if (isServerAction) {
+        // Let it go. The action will handle the error and the React Query will receive the response.
+        return NextResponse.next()
+    }
+
+    // protection against infinite loop
+    if (isPublicRoute) {
+        const response = NextResponse.next()
+        response.cookies.delete('session_token')
+        return response
+    }
+
+    return NextResponse.redirect(new URL('/entrar', request.url))
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
